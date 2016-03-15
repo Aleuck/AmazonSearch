c324ddbfb8fd670e2c2e024affb4dcd9aa3fafaf
Alexandre Leuck <aleuck2004@gmail.com>
2016-03-15 01:05:45 +0100

    complete rewrite for nodejs+phantomjs

---
+++ amazon.js
@@ -0,0 +1,53 @@
+"use strict";
+process.on('uncaughtException', (err) => {
+  console.log(`Caught exception: ${err}`);
+});
+
+process.on('unhandledRejection', (reason, p) => {
+    console.log("Unhandled Rejection at: Promise ", p, " reason: ", reason);
+    // application specific logging, throwing an error, or other logic here
+});
+var child_process = require('child_process')
+var amazonSearch = require('./new');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var fs = require('fs');
+// Lets search the german amazon!
+var search = amazonSearch.create({
+    base_uri: 'http://www.amazon.de/',
+    save_html: true,
+    request_interval: 2000,
+    detailed: true
+});
+
+
+if (process.argv.length === 2) {
+	console.log("please specify the search string");
+}
+var keywords = process.argv.slice(2).join(' ');
+//  handy+h%C3%BClle
+console.log(`Searching amazon for '${keywords}':`);
+// Searching for "cuia" sorted by "review-rank"
+search(keywords, 'review-rank', (result) => {
+    console.log("READY!");
+    var outputName = (new Date(result.date)).toISOString().replace(/:/g, '-') + `.k.${result.keywords}.s.${result.sort}`;
+    console.log('outputName');
+    fs.writeFile(`${outputName}.json`, JSON.stringify(result), (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('file saved.');
+        child_process.execSync(`pdfunite page_* ${outputName}.pdf`);
+        child_process.execSync('rm page_*');
+        process.exit();
+    });
+    var url = 'mongodb://localhost:27017/amazonSearch';
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log('connected to mongodb');
+        var collection = db.collection(result.keywords);
+        collection.insert(result, function (err, result) {
+            assert.equal(null, err);
+        });
+    });
+});
--- amazonsearch.js
+++ amazonsearch.js
@@ -1,36 +1,20 @@
-/*\ AMAZON SEARCH CRAWLER
-
-Author: Alexandre Leuck
-
--------------------------------------------------
-Valid sort options:
--------------------------------------------------
-
- - 'relevancerank';
- - 'featured-rank';
- - 'price-asc-rank';
- - 'price-desc-rank';
- - 'review-rank'; and
- - 'date-desc-rank'.
-
--------------------------------------------------
-\*/
-
 "use strict";
+
+var phantom = require('phantom');
+
 var defaultSettings = {
     base_uri: "http://www.amazon.com/",
     request_interval: 500,
     save_html: false,
     save_screenshots: false,
     window_size: {
-        width: 1280,
-        height: 720
+        width: 1024,
+        height: 768
     },
+    detailed: false,
     user_agent: 'SpecialAgent'
 };
-var htmlCrawler = function () {
-    return document.documentElement.outerHTML;
-};
+
 // function to crawl the number of result pages from document
 var pageCountCrawler = function () {
     // the previous sibling of the "next page" button contains the last page number
@@ -40,10 +24,13 @@ var pageCountCrawler = function () {
     }
     return parseInt(next.previousElementSibling.textContent, 10);
 };
-// function to crawl results' basic data from search results page
+// function to crawl results' basic dadetailedta from search results page
 var resultsDataCrawler = function (settings) {
     var resultsDOM = document.getElementsByClassName("s-result-item");
-    var results = [];
+    var page = {
+        results: [],
+        html: undefined
+    };
     Array.prototype.forEach.call(resultsDOM, function (rElement) {
         var r = {};
         var h2;
@@ -57,30 +44,74 @@ var resultsDataCrawler = function (settings) {
         // find result rating and if it is Prime or not.
         var possibleRatingEls = rElement.getElementsByClassName("a-icon-alt");
         if (possibleRatingEls.length > 0) {
-            r.rating = parseFloat(possibleRatingEls[possibleRatingEls.length - 1].textContent.split(' ')[0].replace(',','.'), 10) || 0;
+            r.rating = parseFloat(possibleRatingEls[possibleRatingEls.length - 1].textContent.split(' ')[0].replace(',', '.'), 10) || 0;
             r.prime = possibleRatingEls[0].textContent.split(' ')[0] === 'Prime';
         } else {
             r.rating = 0;
             r.prime = false;
         }
-        if (settings.save_html) {
-            r.html = rElement.outerHTML;
-        }
         var possiblePriceEl = rElement.getElementsByClassName('a-color-price');
         if (possiblePriceEl.length > 0) {
-            r.price = parseFloat(possiblePriceEl[0].textContent.replace(/[a-zA-Z\ \t\n\r$€]/g, '').replace(',','.'),10) || null;
-            console.log(r.price);
+            r.price = parseFloat(possiblePriceEl[0].textContent.replace(/[a-zA-Z\ \t\n\r$€]/g, '').replace(',', '.'), 10) || null;
         }
-        // TODO: gather more data
-        results.push(r);
+        page.results.push(r);
+        if (settings.save_html) {
+            page.html = document.documentElement.outerHTML;
+        }
     });
-    return results;
+    return page;
 };
+var resultsDetailedDataCrawler = function (settings) {
+    var details = {
+        numReviews: 0,
+        numAnswers: 0
+    };
+    var detailsEl = document.getElementById('prodDetails');
+    var numReviewsEl = document.getElementById('acrCustomerReviewText');
+    var numAnswersEl = document.getElementById('ask_feature_div');
+    var histogramEl = document.getElementById('histogramTable');
+    if (numReviewsEl) {
+        details.numReviews = parseInt(numReviewsEl.textContent.trim(), 10);
+    }
+    if (numAnswersEl) {
+        numAnswersEl = numAnswersEl.getElementsByClassName('askATFLink')[0];
+        if (numAnswersEl) {
+            details.numAnswers = parseInt(numAnswersEl.textContent.trim(), 10);
+        }
+    }
+    if (histogramEl) {
+        var ratingRows = histogramEl.getElementsByTagName('tr');
+        details.numRatings5 = parseInt(ratingRows[0].lastElementChild.textContent.trim(), 10);
+        details.numRatings4 = parseInt(ratingRows[1].lastElementChild.textContent.trim(), 10);
+        details.numRatings3 = parseInt(ratingRows[2].lastElementChild.textContent.trim(), 10);
+        details.numRatings2 = parseInt(ratingRows[3].lastElementChild.textContent.trim(), 10);
+        details.numRatings1 = parseInt(ratingRows[4].lastElementChild.textContent.trim(), 10);
+    }
+    if (detailsEl) {
+        var tdEls, k, v;
+        tdEls = detailsEl.getElementsByTagName('td');
+        k = Array.prototype.filter.call(tdEls, function (e) {
+            return e.classList.contains('label');
+        }).map(function (e) {
+            return e.textContent.trim();
+        });
+        v = Array.prototype.filter.call(tdEls, function (e) {
+            return e.classList.contains('value');
+        }).map(function (e) {
+            return e.textContent.trim();
+        });
+        details.ASIN = v[k.indexOf('ASIN')];
+        details.dateFirstAvailable = v[k.indexOf('Date First Available')];
+    }
+    return details;
+};
+
 exports.defaultSettings = defaultSettings;
 exports.create = function (customSettings) {
     var settings = {};
     var props = Object.keys(defaultSettings);
     var i;
+    customSettings = customSettings || {};
     // get custom settings
     for (i = 0; i < props.length; i += 1) {
         if (customSettings.hasOwnProperty(props[i])) {
@@ -89,81 +120,105 @@ exports.create = function (customSettings) {
             settings[props[i]] = defaultSettings[props[i]];
         }
     }
-    // amazon does not allow sort options if we identify ourselves as "phantomjs"
     var search = function (keywords, sort, callback) {
         keywords = keywords.split(/[\ \t\n\r]+/).map(encodeURIComponent).join('+');
-        var uri = settings.base_uri + 's/ref=nb_sb_noss_2?keywords=' + keywords + '&sort=' + sort;
-        var webpage = require('webpage');
-        var searchPage = webpage.create();
+        var uri = settings.base_uri + 's/ref=nb_sb_noss_2?language=en_GB&keywords=' + keywords + '&sort=' + sort;
         var result = {
             keywords: keywords,
             sort: sort,
             uri: uri,
             pages: [],
             results: [],
-            settings: settings
+            settings: Object.create(settings)
         };
-        searchPage.settings.userAgent = settings.user_agent;
-        searchPage.viewportSize = settings.window_size;
-
-        // for debugging
-        searchPage.onConsoleMessage = function (msg) {
-            console.log("--> Page says: " + msg);
-        };
-        // searchPage.onLoadFinished = function () {
-        //     console.log("-- finished loading --");
-        // };
-
-        //console.log("oppening: ", uri);
-
-        searchPage.open(uri, function (status) {
-            var pageCount, currentPage, openCurrentPage, productsFetcher;
-            if (status !== 'success') {
-                throw new Error("Page couldn't be opened.");
-            }
-            result.date = new Date();
-            // lets find out how many pages of results we got
-            pageCount = searchPage.evaluate(pageCountCrawler, settings);
-            currentPage = 1;
-            openCurrentPage = function () {
-                searchPage.open(uri + '&page=' + currentPage, productsFetcher);
-            };
-            productsFetcher = function () {
-                // take a screenshot
-                if (settings.save_screenshots) {
-                    searchPage.render('page_' + currentPage + '.png');
-                }
-
-                // gather data of each result in this page
-                var page = {};
-                page.date = new Date();
-                page.number = currentPage;
-                page.uri = uri + '&page=' + currentPage;
-                // get data from the results in this page
-                page.results = searchPage.evaluate(resultsDataCrawler, settings);
-                // concat with all the products gathered from previous pages
-                result.results = result.results.concat(page.results);
-                // do we want to save the html?
-                if (settings.save_html) {
-                    page.html = searchPage.evaluate(htmlCrawler);
-                }
-                // add page to the results
-                result.pages.push(page);
-                console.log('Pages read: ' + currentPage + '/' + pageCount);
-                // move to next page, if needed
-                if (currentPage < pageCount) {
-                    currentPage += 1;
-                    setTimeout(openCurrentPage, settings.request_interval);
-                } else {
-                    if (typeof callback === 'function') {
-                        callback(result);
-                    }
-                }
-            };
-            console.log('Pages read: 0/' + pageCount);
-            setTimeout(openCurrentPage, settings.request_interval);
+        phantom.create().then(function (ph) {
+            ph.createPage().then(function (page) {
+                var userAgentPromise = page.setting('userAgent', settings.user_agent);
+                var viewportSizePromise = page.property('viewportSize', settings.window_size);
+                var onConsoleMessagePromise = page.property('onConsoleMessage', function (msg) {
+                    console.log("--> Page says: " + msg);
+                });
+                Promise.all([userAgentPromise, viewportSizePromise, onConsoleMessagePromise]).then(function () {
+                    page.open(uri).then(function (status) {
+                        if (status !== 'success') {
+                            //reject(new Error("Page couldn't be opened."));
+                            throw new Error("Page couldn't be opened.");
+                        }
+                        page.evaluate(pageCountCrawler, settings).then(function (pageCount) {
+                            var currentPage, openCurrentPage, resultsFetcher;
+                            currentPage = 1;
+                            console.log(pageCount + ' pages.');
+                            result.date = new Date();
+                            openCurrentPage = function () {
+                                page.open(uri + '&page=' + currentPage).then(resultsFetcher);
+                            };
+                            resultsFetcher = function (status) {
+                                if (status !== 'success') {
+                                    throw new Error("Page couldn't be opened.");
+                                }
+                                // take a screenshotresults
+                                // NOT YET
+                                var p = {};
+                                result.pages.push(p);
+                                p.date = new Date();
+                                p.number = currentPage;
+                                p.uri = uri + '&page=' + currentPage;
+                                console.log('reading page...');
+                                page.evaluate(resultsDataCrawler, settings).then(function (p_data) {
+                                    p.results = p_data.results;
+                                    p.html = p_data.html;
+                                    result.results = result.results.concat(p_data.results);
+                                    console.log('Pages read: ' + currentPage + '/' + pageCount);
+                                    page.render('page_' + ('00' + currentPage).slice(-3) + '.pdf');
+                                    if (currentPage < pageCount) {
+                                        currentPage += 1;
+                                        setTimeout(openCurrentPage, settings.request_interval);
+                                    } else {
+                                        if (settings.detailed) {
+                                            // detailed
+                                            var currentResult, openCurrentResult, detailsFetcher;
+                                            currentResult = 0;
+                                            openCurrentResult = function () {
+                                                page.open(result.results[currentResult].uri).then(detailsFetcher);
+                                            };
+                                            detailsFetcher = function (status) {
+                                                console.log('ResultDetail: ' + (currentResult + 1) + '/' + result.results.length);
+                                                page.evaluate(resultsDetailedDataCrawler).then(function (details) {
+                                                    result.results[currentResult].ASIN = details.ASIN;
+                                                    result.results[currentResult].dateFirstAvailable = details.dateFirstAvailable;
+                                                    result.results[currentResult].numReviews = details.numReviews;
+                                                    result.results[currentResult].numAnswers = details.numAnswers;
+                                                    result.results[currentResult].numRatings5 = details.numRatings5;
+                                                    result.results[currentResult].numRatings4 = details.numRatings4;
+                                                    result.results[currentResult].numRatings3 = details.numRatings3;
+                                                    result.results[currentResult].numRatings2 = details.numRatings2;
+                                                    result.results[currentResult].numRatings1 = details.numRatings1;
+                                                    currentResult += 1;
+                                                    if (currentResult < result.results.length) {
+                                                        page.stop();
+                                                        setTimeout(openCurrentResult, settings.request_interval);
+                                                    } else {
+                                                        console.log('callback');
+                                                        callback(result);
+                                                        //ph.exit();
+                                                    }
+                                                });
+                                            };
+                                            setTimeout(openCurrentResult, settings.request_interval);
+                                        } else {
+                                            console.log('callback');
+                                            callback(result);
+                                            //ph.exit();
+                                        }
+                                    } // else
+                                }); // Promise
+                            }; // resultsFetcher
+                            setTimeout(openCurrentPage, settings.request_interval);
+                        });
+                    });
+                });
+            });
         });
     };
-    search.settings = settings;
     return search;
 };
